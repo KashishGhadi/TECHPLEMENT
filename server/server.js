5ebb389e4353e923ead16c234eff829ad039a8e1
Kashish Ghadi <kashishghadi16@gmail.com>
2024-07-01 18:19:21 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const quotesRoute = require('./routes/quotes');  // Ensure this path is correct
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/quotes', quotesRoute);
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/quotesDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/quotes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Quotes API');
+});
+
+const quoteRoutes = require('./routes/quotes');
+app.use('/api/quotes', quoteRoutes);
 
 // Start the server
 app.listen(PORT, () => {
